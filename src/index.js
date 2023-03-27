ff0be84f3ee388f52e4289a967a08db1d1fbfdd5
Jakub <stankiewiczj88nt@gmail.com>
2023-03-27 11:18:12 +0200

    27.03 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { render } from "react-dom";
 import App from "./App";
 import React from "react";
-
 import "./style/index.scss";
+import OgImg from "./images/ogimage.png";
+<meta property="og:image" content={OgImg} />;
 
 render(
   <App />,
